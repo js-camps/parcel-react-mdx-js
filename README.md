b1d2bf62d35c4952313dcb80a5bfa76652df6fb9
rjgeng <rj.geng@gmail.com>
2024-08-10 17:07:41 -0700

    initial parcel to render react for mdx

---
+++ README.md
@@ -0,0 +1,13 @@
+# parcel react mdx js
+
+```
+npm i --legacy-peer-deps 
+```
+
+or
+
+```
+yarn
+```
+
+
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// src/components/App.js
+import React from 'react';
+import Hello from './hello.mdx';
+
+export function App() {
+  return (
+    <div>
+      <h1>MDX Example</h1>
+      <Hello />
+    </div>
+  );
+}
